3f952ce88ca0466ef62d9541a715e55358fa4afe
ziuwuchuoi <vv0320nt@gmail.com>
2023-12-19 13:40:36 +0700

    ???

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { pathsToModuleNameMapper } = require('ts-jest');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { compilerOptions } = require('./tsconfig.json');
 
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>/src/test'],
   transform: {
+    '^.+\\.ts?$': 'ts-jest',
     '^.+\\.tsx?$': 'ts-jest',
   },
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src' }),
