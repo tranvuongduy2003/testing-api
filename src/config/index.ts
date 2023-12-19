de940343e14caadf6aa508b0cf6277c4d089b44c
Tran Vuong Duy <tranvuongduy2003@gmail.com>
2023-12-20 03:48:41 +0700

    save changes

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 import { config } from 'dotenv';
 
-config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
 export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN, HOST_NAME } = process.env;
