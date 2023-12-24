9a1cf7dde1b2a37429aea1af6be06d6d6b80524e
Tran Vuong Duy <tranvuongduy2003@gmail.com>
2023-12-25 00:28:42 +0700

    save changes

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,3 +5,4 @@ config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
 export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN, HOST_NAME } = process.env;
 export const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE, DB_PASS } = process.env;
+export const { TEST_TOKEN } = process.env;
